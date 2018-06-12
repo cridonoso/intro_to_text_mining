3ef1d3f6d19a1833e1f298050d8e1a247c9d01d1
Cristobal <cridonoso@udec.cl>
2018-06-12 14:16:48 -0400

    update

--- pred_model/script.py
+++ pred_model/script.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
-
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+import numpy as np
 from Bio import Entrez
 
 def search(query):
@@ -14,12 +15,27 @@ def search(query):
     return results
 
 def print_abstract(pmid):
-    handle = Entrez.efetch(db='pubmed', id=pmid, retmode='text', rettype='abstract')
-    print handle.read()
+    handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml', rettype='abstract')
+    return handle.read()
 
 query = 'cancer'
 idList = search(query)['IdList']
-for pid in idList:
+for pid in idList[0:3]:
     print '-'*60
-    print_abstract(pid)
+    xml = str(print_abstract(pid))
+    find = re.findall(r"<AbstractText[>]?.*</AbstractText>", xml, re.U | re.I | re.S)
+    text = re.sub(r'<[^<]+>', "", str(find), re.U)
+    text = ''.join([t for t in text])
+
+    patterns = r'\w+'
+    text = ' '.join(re.findall(patterns, text))
+    #print text
+
+    tokens = word_tokenize(text)
+    tokens = [token.lower() for token in tokens]
+
+    uniques = np.unique(tokens)
+    print uniques
+
+
 
