861b0b19bd8d99d15e1a0fe3b71914df6b5ff5a3
Cristobal <cridonoso@udec.cl>
2018-06-12 16:56:34 -0400

    text without parentesis are generated

--- pred_model/script.py
+++ pred_model/script.py
@@ -14,28 +14,76 @@ def search(query):
     results = Entrez.read(handle)
     return results
 
-def print_abstract(pmid):
+def get_abstract(pmid):
     handle = Entrez.efetch(db='pubmed', id=pmid, retmode='xml', rettype='abstract')
     return handle.read()
 
-query = 'cancer'
-idList = search(query)['IdList']
-for pid in idList[0:3]:
-    print '-'*60
-    xml = str(print_abstract(pid))
-    find = re.findall(r"<AbstractText[>]?.*</AbstractText>", xml, re.U | re.I | re.S)
+def remove_text_inside_brackets(text, brackets="()"):
+    count = [0] * (len(brackets) // 2) # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b: # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                if count[kind] < 0: # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else: # character is not a [balanced] bracket
+            if not any(count): # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars)
+
+def get_sentences(text):
+    find = re.findall(r"<AbstractText[>]?.*</AbstractText>", text, re.U | re.I | re.S)
     text = re.sub(r'<[^<]+>', "", str(find), re.U)
     text = ''.join([t for t in text])
+    print remove_text_inside_brackets(text)
+    text_splitted =  text.split('.')
 
     patterns = r'\w+'
-    text = ' '.join(re.findall(patterns, text))
-    #print text
+    senteces = []
+    for text in text_splitted:
+        text_new = ' '.join(re.findall(patterns, text))
 
-    tokens = word_tokenize(text)
-    tokens = [token.lower() for token in tokens]
+        tokens = word_tokenize(text_new)
+        tokens = [token.lower() for token in tokens]
+        senteces.append(tokens)
 
-    uniques = np.unique(tokens)
-    print uniques
+    for i,(tokens) in enumerate(senteces):
+        for j,(token) in enumerate(tokens):
+            if (token == 'xc2') or (token == 'xa9'):
+                del senteces[i][j]
+
+    return senteces
+
+def standarize_numbers(sentences):
+    for i,(tokens) in enumerate(sentences):
+        for j,(u) in enumerate(tokens):
+            if re.match(r'[0-9]+', u):
+                if len(u) == 4 and u[0]!='0':
+                    sentences[i][j] = '2018'
+                else:
+                    sentences[i][j] = '1'
+    return sentences
+
+def generate_model(query, ngrams):
+
+    idList = search(query)['IdList']
+    resources = []
+    for i,(pid) in enumerate(idList):
+        print 'reading abstract number',i
+        xml = str(get_abstract(pid))
+        sentences = get_sentences(xml)
+        standar = standarize_numbers(sentences)
+        #print standar
+        uniques = np.unique(standar)
+        resources.append(uniques)
+    #unique_words =  len(list(set().union(resources[0],resources[1],resources[2])))
+
+generate_model('cancer', 2)
+
 
 
 
